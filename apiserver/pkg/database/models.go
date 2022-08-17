5e8d7d188f50fcaf133b7727467d1d1bab26b840
SeroHsueh <cello1124@gmail.com>
2022-08-17 17:19:01 +0800

    feat: design Link model

--- apiserver/pkg/database/models.go
+++ apiserver/pkg/database/models.go
@@ -3,14 +3,19 @@ package database
 import (
 	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Link struct {
-	gorm.Model
-	ID        int64     `gorm:"primary_key;auto_increment" json:"id"`
-	Url       string    `gorm:"size:100;not null;unique" json:"url"`
-	Code      string    `gorm:"size:100;not null;" json:"code"`
-	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
-	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+	ID        uuid.UUID `gorm:"primaryKey;type:uuid" json:"id"`
+	Url       string    `gorm:"size:256;not null;unique" json:"url"`
+	Code      string    `gorm:"size:50;not null;" json:"code"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime:milli" json:"updated_at"`
+}
+
+func (link *Link) BeforeCreate(tx *gorm.DB) (err error) {
+	link.ID = uuid.New()
+	return
 }
